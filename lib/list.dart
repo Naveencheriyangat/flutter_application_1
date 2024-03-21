531981dc024de22ccfe6ae63089847502b49b2a0
Naveencheriyangat <naveenkkut128@gmail.com>
2024-03-21 09:03:15 +0530

    code changed

--- lib/list.dart
+++ lib/list.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const PatientListPage());
+void main() {
+  runApp(PatientListPage());
+}
 
 class PatientListPage extends StatelessWidget {
-  const PatientListPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,15 +13,148 @@ class PatientListPage extends StatelessWidget {
         primarySwatch: Colors.red,
         scaffoldBackgroundColor: Color.fromARGB(255, 240, 251, 255),
       ),
-      home: const Login(),
+      home: MyHomePage(),
     );
   }
 }
 
-class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('Cardio ', style: TextStyle(color: Colors.black)),
+            Text('Vista', style: TextStyle(color: Color(0xFFAA1F24))),
+          ],
+        ),
+        elevation: 5,
+        shadowColor: Colors.black,
+      ),
+      body: PatientList(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.home, color: Colors.white),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.person, color: Colors.white),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
 
-  String? get name => null;
+class PatientList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            spreadRadius: 5,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20, top: 20), // Adjust left padding
+              child: Text(
+                'Patient List',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Color(0xFFAA1F24),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            buildPatientButton('Adheena', '02/02/2002', context),
+            buildPatientButton('Swetha', '05/08/2002', context),
+            buildPatientButton('Vishnu', '02/05/2002', context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPatientButton(String name, String date, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      height: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Color(0xFFAA1F24), width: 2),
+      ),
+      child: Material(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(20),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AnotherPage(name: name, date: date)),
+            );
+          },
+          splashColor: Color(0xFFAA1F24),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Color(0xFFAA1F24),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  date,
+                  style: TextStyle(
+                    color: Colors.grey, // Different color for date
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnotherPage extends StatelessWidget {
+  final String name;
+  final String date;
+
+  AnotherPage({required this.name, required this.date});
 
   @override
   Widget build(BuildContext context) {
@@ -65,11 +198,11 @@ class Login extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  name ?? '',
+                  name,
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
-                    color: Color(0xFFAA1F24),
+                    color:  Color(0xFFAA1F24),
                   ),
                 ),
               ],
@@ -83,13 +216,13 @@ class Login extends StatelessWidget {
             ),
             SizedBox(height: 20),
             ProfileInfoField(label: 'Age', value: '22'),
-            ProfileInfoField(label: 'Weight', value: '70 kg'),
-            ProfileInfoField(label: 'Height', value: '170 cm'),
+            ProfileInfoField(label: 'Weight', value: '0'),
+            ProfileInfoField(label: 'Height', value: '0'),
             ProfileInfoField(label: 'Hyper tension/BP', value: 'No'),
             ProfileInfoField(label: 'Sex', value: 'Female'),
             ProfileInfoField(label: 'Chest pain', value: 'No'),
             ProfileInfoField(label: 'Surgery', value: 'No'),
-            ProfileInfoField(label: 'Diseases', value: 'None'),
+            ProfileInfoField(label: 'Diseases', value: 'No'),
           ],
         ),
       ),
@@ -124,39 +257,39 @@ class ProfileInfoField extends StatelessWidget {
   final String label;
   final String value;
 
-  const ProfileInfoField({Key? key, required this.label, required this.value})
-      : super(key: key);
+  ProfileInfoField({required this.label, required this.value});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Text(
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      padding: EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
             label,
             style: TextStyle(
               fontSize: 18,
               fontWeight: FontWeight.bold,
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            border: Border.all(color: Colors.grey),
-          ),
-          child: Text(
+          Text(
             value,
             style: TextStyle(
               fontSize: 18,
             ),
           ),
-        ),
-        SizedBox(height: 10),
-      ],
+        ],
+      ),
     );
   }
 }
