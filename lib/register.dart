273b6c3ff07fde7348b65f30fc649485d16fcf79
Naveencheriyangat <naveenkkut128@gmail.com>
2024-04-06 09:50:19 +0530

    field changed

--- lib/register.dart
+++ lib/register.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'signin.dart';
 
-
 void main() => runApp(const RegisterPage());
 
 class RegisterPage extends StatelessWidget {
@@ -73,190 +72,192 @@ class _MyLoginPageState extends State<MyLoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'SIGN UP',
-            style: TextStyle(
-              color:Color(0xFFAA1F24),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              shadows: [
-                Shadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: Offset(2, 2),
-                  blurRadius: 2,
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 16),
-          Text('Don\'t have an account? Create your account, it takes less than a minute'),
-          SizedBox(height: 16),
-          Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFFAA1F24)),
-                    borderRadius: BorderRadius.circular(10),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'SIGN UP',
+              style: TextStyle(
+                color: Color(0xFFAA1F24),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    color: Colors.black.withOpacity(0.5),
+                    offset: Offset(2, 2),
+                    blurRadius: 2,
                   ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'First Name',
-                       labelStyle: TextStyle(color: Color(0xFFAA1F24)),
-                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFFAA1F24)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Last Name',
-                       labelStyle: TextStyle(color: Color(0xFFAA1F24)),
-                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFFAA1F24)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                       labelStyle: TextStyle(color: Color(0xFFAA1F24)),
-                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFFAA1F24)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                       labelStyle: TextStyle(color: Color(0xFFAA1F24)),
-                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      border: InputBorder.none,
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFFAA1F24)),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                       labelStyle: TextStyle(color: Color(0xFFAA1F24)),
-                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      border: InputBorder.none,
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: [
-                    Checkbox(
-                      value: true, // Replace with your logic for accepting terms
-                      onChanged: (bool? value) {
-                        // Handle checkbox state
-                      },
-                    ),
-                    Text('I accept the terms and privacy policy'),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: MouseRegion(
-                    onEnter: (_) => setState(() => _isSignInHovered = true),
-                    onExit: (_) => setState(() => _isSignInHovered = false),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SignIn()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: _isSignInHovered ? Colors.black : Colors.white,
-                        backgroundColor: _isSignInHovered ? Color(0xFFAA1F24) : Color.fromARGB(255, 240, 251, 255),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: BorderSide(color: Color(0xFFAA1F24)),
-                        ),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Text(
-                          'Next',
-                          style: TextStyle(color: Colors.black), // Set text color to black
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: MouseRegion(
-                    onEnter: (_) => setState(() => _isBackHovered = true),
-                    onExit: (_) => setState(() => _isBackHovered = false),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Login()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: _isBackHovered ? Colors.black : Colors.white,
-                        backgroundColor: _isBackHovered ? Color(0xFFAA1F24) : Color.fromARGB(255, 240, 251, 255),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: BorderSide(color: Color(0xFFAA1F24)),
-                        ),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(12.0),
-                        child: Text(
-                          'Back',
-                          style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                ),
+                ],
               ),
-            ],
-          ),
-        )
-        ],
+            ),
+            SizedBox(height: 16),
+            Text('Don\'t have an account? Create your account, it takes less than a minute'),
+            SizedBox(height: 16),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFAA1F24)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'First Name',
+                        labelStyle: TextStyle(color: Color(0xFFAA1F24)),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFAA1F24)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Last Name',
+                        labelStyle: TextStyle(color: Color(0xFFAA1F24)),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFAA1F24)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color(0xFFAA1F24)),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFAA1F24)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Color(0xFFAA1F24)),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        border: InputBorder.none,
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xFFAA1F24)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Confirm Password',
+                        labelStyle: TextStyle(color: Color(0xFFAA1F24)),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        border: InputBorder.none,
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: true, // Replace with your logic for accepting terms
+                        onChanged: (bool? value) {
+                          // Handle checkbox state
+                        },
+                      ),
+                      Text('I accept the terms and privacy policy'),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: MouseRegion(
+                      onEnter: (_) => setState(() => _isSignInHovered = true),
+                      onExit: (_) => setState(() => _isSignInHovered = false),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SignIn()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: _isSignInHovered ? Colors.black : Colors.white,
+                          backgroundColor: _isSignInHovered ? Color(0xFFAA1F24) : Color.fromARGB(255, 240, 251, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Color(0xFFAA1F24)),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Next',
+                            style: TextStyle(color: Colors.black), // Set text color to black
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: MouseRegion(
+                      onEnter: (_) => setState(() => _isBackHovered = true),
+                      onExit: (_) => setState(() => _isBackHovered = false),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: _isBackHovered ? Colors.black : Colors.white,
+                          backgroundColor: _isBackHovered ? Color(0xFFAA1F24) : Color.fromARGB(255, 240, 251, 255),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Color(0xFFAA1F24)),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Back',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
